13b4a35345c9bbef77801919cffebb75288254e6
tushar0837 <tushar0837@gmail.com>
2018-11-18 13:29:20 +0530

    IE issue fixed

--- assets/jss/material-kit-react/components/headerStyle.jsx
+++ assets/jss/material-kit-react/components/headerStyle.jsx
@@ -42,7 +42,6 @@ const headerStyle = {
   container: {
     ...container,
     minHeight: "50px",
-    flex: "1",
     alignItems: "center",
     justifyContent: "space-between",
     display: "flex",
